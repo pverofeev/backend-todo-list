6a7d7c7ed5e1ea5985e47b19641d833c861728ce
Nikita Elfimov <ne@atls.team>
2022-09-28 15:02:59 +0500

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -37,7 +37,6 @@ object Build : BuildType({
     name = "Build 111"
 
     params {
-        param("env.GH_TOKEN", "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")
         param("env.GH_USERNAME", "TFK70")
     }
 
